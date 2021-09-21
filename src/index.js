dc292f7c421a54c5b5a91eea2ab01f9237477373
Lunopark <rostypod@gmail.com>
2021-09-21 18:08:48 +0300

    install logger & thunk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
-import './styles.css'
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { rootReducer } from "./redux/rootReducer";
+import { asyncIncrement, changeTheme, decrement, increment } from "./redux/actions";
+import './styles.css'
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -8,29 +11,44 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const store = createStore(rootReducer, 0)
+// Logger
+// function logger(state) {
+//   return function(next) {
+//     return function(action) {
+//       console.log('previous state: ', state.getState())
+//       console.log('action: ', action)
+//       const newState = next(action)
+//       console.log('new state: ', newState)
+
+//       return newState
+//     }
+//   }
+// }
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
+  store.dispatch(increment())
 })
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'DECREMENT' })
+  store.dispatch(decrement())
 })
 
 asyncBtn.addEventListener('click', () => {
-
+  store.dispatch(asyncIncrement())
 })
 
 store.subscribe(() => {
   const state = store.getState()
 
-  counter.textContent = state
+  counter.textContent = state.counter
+  document.body.className = state.theme.value
 })
 
 store.dispatch({type: 'INIT_APPLICATION'})
 
 themeBtn.addEventListener('click', () => {
-  // document.body.classList.toggle('dark')
+  const newTheme = document.body.classList.contains('light') ? 'dark' : 'light'
+  store.dispatch(changeTheme(newTheme))
 })
-
